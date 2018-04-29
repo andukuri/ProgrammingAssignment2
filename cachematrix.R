0219157cbc030ce9112179726cd9773c2f6dfe3f
Poornima S Andukuri <p.andukuri@gmail.com>
2018-04-29 07:33:49 -0600

    Add files via upload
    Programming assignment for Week 3 in R Programming. cachematric.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,40 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(matx = matrix()) {
+  matinv <- NULL
+  ## set the matrix at global
+  set <- function(maty){
+    matx <<- maty
+    matinv <<- NULL
+  }
+  ## get function to read the matrix
+  get <-function() return(matx)
+  ## set inverse value 
+  setinverse <- function(maty) matinv <<- maty
+  ## get inverse value
+  getinverse <- function() return(matinv)
+  return(list(set=set, get=get, setinverse=setinverse, getinverse=getinverse))
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(matX, ...) {
         ## Return a matrix that is the inverse of 'x'
+  ## call getinversion function from makeCacheMatrix
+  matinv<-matX$getinverse()
+  
+  if(!is.null(matinv))
+  {## if cached data exists
+    message("getting cached data")
+    return(matinv)
+  }
+  ## if no cached data
+  data <- matX$get()
+  ## calculate inverse of matrix
+  matinv <-solve(data,...)
+  ## call setinverse to set global value
+  matX$setinverse(matinv)
+  return(matinv)
 }
